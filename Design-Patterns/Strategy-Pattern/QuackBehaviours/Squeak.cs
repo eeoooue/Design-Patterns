c9c579583a57acb6c994e3596cb0798a79887265
eeoooue <pdmoorhouse@gmail.com>
2023-05-07 23:05:01 +0100

    created QuackBehaviour children

---
+++ Design-Patterns/Strategy-Pattern/QuackBehaviours/Squeak.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_Pattern.QuackBehaviours
+{
+    internal class Squeak : QuackBehaviour
+    {
+        public void Quack()
+        {
+            Console.WriteLine($"The duck squeaked. 'Squeak!'");
+        }
+    }
+}
