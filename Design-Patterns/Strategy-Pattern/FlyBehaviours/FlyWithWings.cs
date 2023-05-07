8be0cfdef7d1644e264d3179d0793ffeb4eb0011
eeoooue <pdmoorhouse@gmail.com>
2023-05-07 23:04:44 +0100

    created FlyBehaviour children

---
+++ Design-Patterns/Strategy-Pattern/FlyBehaviours/FlyWithWings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_Pattern.FlyBehaviours
+{
+    internal class FlyWithWings : FlyBehaviour
+    {
+        public void Fly()
+        {
+            Console.WriteLine($"The duck flew!");
+        }
+    }
+}
