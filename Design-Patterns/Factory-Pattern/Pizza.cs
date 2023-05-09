2e7e1fce2e00d780876477bb4b50762dd39413f0
eeoooue <pdmoorhouse@gmail.com>
2023-05-09 22:54:21 +0100

    writing up Pizza example from HFDP p150

---
+++ Design-Patterns/Factory-Pattern/Pizza.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Pattern
+{
+    internal class Pizza
+    {
+
+        public void Prepare()
+        {
+            
+        }
+
+        public void Bake()
+        {
+            
+        }
+
+        public void Cut()
+        {
+            
+        }
+
+        public void Box()
+        {
+            
+        }
+    }
+}
---
+++ Design-Patterns/Factory-Pattern/PizzaShop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Pattern
+{
+    internal class PizzaShop
+    {
+
+
+        public Pizza OrderPizza()
+        {
+            Pizza pizza = new Pizza();
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+    }
+}
