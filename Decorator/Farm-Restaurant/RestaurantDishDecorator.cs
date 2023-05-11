bb80962550516743868f33fbe2e50079f11f3fad
eeoooue <pdmoorhouse@gmail.com>
2023-05-11 09:31:07 +0100

    copied-across FarmRestaurant example from DailyDesignPattern

---
+++ Decorator/Farm-Restaurant/RestaurantDishDecorator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm_Restaurant
+{
+    /// <summary>
+    /// The abstract Decorator class.  
+    /// </summary>
+    abstract class Decorator : RestaurantDish
+    {
+        protected RestaurantDish _dish;
+
+        public Decorator(RestaurantDish dish)
+        {
+            _dish = dish;
+        }
+
+        public override void Display()
+        {
+            _dish.Display();
+        }
+    }
+
+    /// <summary>
+    /// A ConcreteDecorator. This class will impart "responsibilities" onto the dishes 
+    /// (e.g. whether or not those dishes have enough ingredients left to order them)
+    /// </summary>
+    class Available : Decorator
+    {
+        public int NumAvailable { get; set; } //How many can we make?
+        protected List<string> customers = new List<string>();
+        public Available(RestaurantDish dish, int numAvailable) : base(dish)
+        {
+            NumAvailable = numAvailable;
+        }
+
+        public void OrderItem(string name)
+        {
+            if (NumAvailable > 0)
+            {
+                customers.Add(name);
+                NumAvailable--;
+            }
+            else
+            {
+                Console.WriteLine("\nNot enough ingredients for " + name + "'s order!");
+            }
+        }
+
+        public override void Display()
+        {
+            base.Display();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Ordered by " + customer);
+            }
+        }
+    }
+}
