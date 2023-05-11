22d51c78c0d9c5cd478a976b53b1fe047648c97f
eeoooue <pdmoorhouse@gmail.com>
2023-05-11 22:43:24 +0100

    copied CandyCollection example from DailyDesignPattern

---
+++ Iterator/Candy-Collection/JellyBean.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candy_Collection
+{
+    /// <summary>
+    /// Our collection item.  Mostly because I'm a sucker for jelly beans.
+    /// </summary>
+    class JellyBean
+    {
+        private string _flavor;
+
+        // Constructor
+        public JellyBean(string flavor)
+        {
+            this._flavor = flavor;
+        }
+
+
+        public string Flavor
+        {
+            get { return _flavor; }
+        }
+    }
+}
---
+++ Iterator/Candy-Collection/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Candy_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build a collection of jelly beans
+            JellyBeanCollection collection = new JellyBeanCollection();
+            collection[0] = new JellyBean("Cherry");
+            collection[1] = new JellyBean("Bubble Gum");
+            collection[2] = new JellyBean("Root Beer");
+            collection[3] = new JellyBean("French Vanilla");
+            collection[4] = new JellyBean("Licorice");
+            collection[5] = new JellyBean("Buttered Popcorn");
+            collection[6] = new JellyBean("Juicy Pear");
+            collection[7] = new JellyBean("Cinnamon");
+            collection[8] = new JellyBean("Coconut");
+
+            // Create iterator
+            IJellyBeanIterator iterator = collection.CreateIterator();
+
+            Console.WriteLine("Gimme all the jelly beans!");
+
+            for (JellyBean item = iterator.First(); !iterator.IsDone; item = iterator.Next())
+            {
+                Console.WriteLine(item.Flavor);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
