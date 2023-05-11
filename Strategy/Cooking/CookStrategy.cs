426996ca2f6f1d84bacd63f5c801b469fd600d64
eeoooue <pdmoorhouse@gmail.com>
2023-05-11 12:45:57 +0100

    created abstract CookStrategy

---
+++ Strategy/Cooking/CookStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cooking
+{
+    abstract class CookStrategy
+    {
+        /// <summary>
+        /// The Strategy abstract class, which defines an 
+        /// interface common to all supported strategy algorithms.
+        /// </summary>
+        public abstract void Cook(string food);
+    }
+}
