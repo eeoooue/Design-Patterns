c190e8a02776fce698aaccb0e91b79029efdbf03
eeoooue <pdmoorhouse@gmail.com>
2023-05-11 14:40:06 +0100

    trying FoodMarket Observer exercise from DailyDesignPattern
    https://www.exceptionnotfound.net/observer-pattern-in-csharp/

---
+++ Observer/Food-Market/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Food_Market
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
