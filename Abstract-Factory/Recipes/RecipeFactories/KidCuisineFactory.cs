4aad042f01b07adc737761f8a2b939c7a65b66ae
eeoooue <pdmoorhouse@gmail.com>
2023-05-11 23:32:38 +0100

    copied Recipes example from DailyDesignPattern

---
+++ Abstract-Factory/Recipes/RecipeFactories/KidCuisineFactory.cs
@@ -0,0 +1,26 @@
+﻿using Recipes.Deserts;
+using Recipes.Sandwiches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.RecipeFactories
+{
+    /// <summary>
+    /// A concrete factory which creates concrete objects by implementing the abstract factory's methods.
+    /// </summary>
+    class KidCuisineFactory : RecipeFactory
+    {
+        public override Sandwich CreateSandwich()
+        {
+            return new GrilledCheese();
+        }
+
+        public override Dessert CreateDessert()
+        {
+            return new IceCreamSundae();
+        }
+    }
+}
---
+++ Abstract-Factory/Recipes/Sandwiches/BLT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.Sandwiches
+{
+    /// <summary>
+    /// A ConcreteProduct
+    /// </summary>
+    class BLT : Sandwich { }
+}
