a7f608d31d3650f2436b620e37cdf95a172f1253
eeoooue <pdmoorhouse@gmail.com>
2023-05-11 14:43:42 +0100

    copied up FoodMarket example

---
+++ Observer/Food-Market/IRestaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food_Market
+{
+    /// <summary>
+    /// The Observer interface
+    /// </summary>
+    interface IRestaurant
+    {
+        void Update(Vegetable veggies);
+    }
+}
--- Observer/Food-Market/Program.cs
+++ Observer/Food-Market/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Food_Market
+﻿using Food_Market.Vegetables;
+
+namespace Food_Market
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create price watch for Carrots 
+            // and attach restaurants that buy carrots from suppliers.
+            Carrots carrots = new Carrots(0.82);
+            carrots.Attach(new Restaurant("Mackay's", 0.77));
+            carrots.Attach(new Restaurant("Johnny's Sports Bar", 0.74));
+            carrots.Attach(new Restaurant("Salad Kingdom", 0.75));
+
+            // Fluctuating carrot prices will notify subscribing restaurants.
+            carrots.PricePerPound = 0.79;
+            carrots.PricePerPound = 0.76;
+            carrots.PricePerPound = 0.74;
+            carrots.PricePerPound = 0.81;
+
+            Console.ReadKey();
         }
     }
 }
