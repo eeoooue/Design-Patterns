e25680725fabf9f65f97e63bc290732bb216c0bb
eeoooue <pdmoorhouse@gmail.com>
2023-05-13 00:39:25 +0100

    wrote-up Pizza-Ingredients exercise from HFDP

---
+++ Abstract-Factory/Pizza-Ingredients/PizzaIngredientFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_Ingredients
+{
+    interface PizzaIngredientFactory
+    {
+        abstract Dough CreateDough();
+
+        abstract Sauce createSauce();
+
+        abstract Cheese createCheese();
+    }
+}
