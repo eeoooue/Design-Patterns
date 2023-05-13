f2a443eb88a2ba52c5e057477f3b9bc18bce8dfd
eeoooue <pdmoorhouse@gmail.com>
2023-05-13 20:03:59 +0100

    added Waitress from example

---
+++ Iterator/Cafe-Menu/Waitress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cafe_Menu
+{
+    internal class Waitress
+    {
+        public void PrintMenu() { }
+        // - prints every item on the menu
+        public void PrintBreakfastMenu() { }
+        // - prints just breakfast items
+        public void PrintLunchMenu() { }
+        // - prints just lunch items
+        public void PrintVegetarianMenu() { }
+        // - prints all vegetarian menu items
+        public void IsItemVegetarian(string name) { }
+        // - given the name of an item, returns true
+        // if the items is vegetarian, otherwise,
+        // returns false
+    }
+}
