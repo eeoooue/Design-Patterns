591d5ccdc95012728e9d5a7eb8e0dc5efde65ae6
eeoooue <pdmoorhouse@gmail.com>
2023-05-22 16:50:20 +0100

    started Geekific restaurant example

---
+++ Factory-Method/Restaurant/Restaurant.cs
@@ -0,0 +1,30 @@
+﻿using Restaurant.Burgers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    internal class Restaurant
+    {
+
+        public Burger OrderBurger(string request)
+        {
+            Burger burger;
+
+            if (request == "veggie")
+            {
+                burger = new VeggieBurger();
+            }
+            else
+            {
+                burger = new BeefBurger();
+            }
+
+            burger.Prepare();
+            return burger;
+        }
+    }
+}
