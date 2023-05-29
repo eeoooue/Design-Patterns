12531a7f3a5c25aa1a3e96e5cfb2aa3785d04154
eeoooue <pdmoorhouse@gmail.com>
2023-05-29 01:47:34 +0100

    created abstract Pizza class with Cost & Description properties

---
+++ Decorator/Pizza/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    abstract class Pizza
+    {
+        public double Cost { get; set; }
+        public string Description {get; set; }
+
+        public Pizza(string description, double cost)
+        {
+            Cost = cost;
+            Description = description;
+        }
+    }
+}
