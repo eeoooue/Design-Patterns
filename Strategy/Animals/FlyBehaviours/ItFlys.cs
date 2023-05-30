2568f757e89ae7e0a158ff82f1ea863afabda5ca
eeoooue <pdmoorhouse@gmail.com>
2023-05-30 04:40:51 +0100

    created classes to store different Fly strategies

---
+++ Strategy/Animals/FlyBehaviours/ItFlys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.FlyBehaviours
+{
+    internal class ItFlys : Flys
+    {
+        public string Fly()
+        {
+            return "Flying High";
+        }
+    }
+}
