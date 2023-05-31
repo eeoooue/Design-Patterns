f4d8d63235c3a32df17097050189345260983888
eeoooue <pdmoorhouse@gmail.com>
2023-05-31 04:43:25 +0100

    added demo/example code to show observer pattern

--- Observer/Stock-Market/Program.cs
+++ Observer/Stock-Market/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Stock_Market
+﻿using Stock_Market.Observers;
+using Stock_Market.Subjects;
+
+namespace Stock_Market
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            StockGrabber grabber = new StockGrabber();
+            grabber.setAAPLPrice(10.00);
+            grabber.setGOOGPrice(10.00);
+            grabber.setIBMPrice(10.00);
+
+            StockObserver observer1 = new StockObserver(grabber);
+            StockObserver observer2 = new StockObserver(grabber);
+
+            grabber.setGOOGPrice(16.00);
         }
     }
 }
