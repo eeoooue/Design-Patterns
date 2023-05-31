60366465fcef77027b76d5f09d8f1e7459c42fc6
eeoooue <pdmoorhouse@gmail.com>
2023-05-31 04:25:09 +0100

    created StockGrabber class which implements Subject

---
+++ Observer/Stock-Market/Subjects/StockGrabber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Market.Subjects
+{
+    internal class StockGrabber : Subject
+    {
+        private List<Observer> _observers;
+
+        public StockGrabber()
+        {
+            _observers = new List<Observer>();
+        }
+
+
+        public void NotifyObservers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Observer observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unregister(Observer observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
