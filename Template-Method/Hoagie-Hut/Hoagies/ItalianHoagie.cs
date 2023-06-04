414b465d3ad50d2ce074613afef7656fd37e62b8
eeoooue <pdmoorhouse@gmail.com>
2023-06-04 05:57:23 +0100

    created abstract Hoagie & ItalianHoagie child class

---
+++ Template-Method/Hoagie-Hut/Hoagies/ItalianHoagie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoagie_Hut.Hoagies
+{
+    internal class ItalianHoagie : Hoagie
+    {
+
+        public void MakeSandwich()
+        {
+            CutBun();
+            AddMeat();
+            AddCheese();
+            AddVegetables();
+            AddCondiments();
+            WrapTheHoagie();
+        }
+
+        public void CutBun()
+        {
+
+        }
+
+        public void AddMeat()
+        {
+
+        }
+
+        public void AddCheese()
+        {
+
+        }
+
+        public void AddVegetables()
+        {
+
+
+        }
+
+        public void AddCondiments()
+        {
+
+        }
+
+        public void WrapTheHoagie()
+        {
+
+        }
+
+    }
+}
