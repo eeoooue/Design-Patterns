e351fb7442843257b0c2493b59c896558e39f1e6
eeoooue <pdmoorhouse@gmail.com>
2023-06-04 06:12:17 +0100

    ItalianHoagie implements abstract methods of Hoagie

--- Template-Method/Hoagie-Hut/Hoagies/ItalianHoagie.cs
+++ Template-Method/Hoagie-Hut/Hoagies/ItalianHoagie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,47 +9,61 @@ namespace Hoagie_Hut.Hoagies
 {
     internal class ItalianHoagie : Hoagie
     {
+        private string[] meatUsed = { "Salami", "Pepperoni", "Capicola Ham" };
 
-        public void MakeSandwich()
+        private string[] cheeseUsed = { "Provolone" };
+
+        private string[] veggiesUsed = { "Lettuce", "Tomatoes", "Onions", "Sweet Peppers" };
+
+        private string[] condimentsUsed = { "Oil", "Vinegar" };
+
+        public override void AddCheese()
         {
-            CutBun();
-            AddMeat();
-            AddCheese();
-            AddVegetables();
-            AddCondiments();
-            WrapTheHoagie();
+            Console.Write($"Adding the cheese:");
+
+            foreach (string cheese in cheeseUsed)
+            {
+                Console.Write($" {cheese}");
+            }
+
+            Console.WriteLine();
         }
 
-        public void CutBun()
+        public override void AddCondiments()
         {
+            Console.Write($"Adding the condiments:");
 
+            foreach (string condiment in condimentsUsed)
+            {
+                Console.Write($" {condiment}");
+            }
+
+            Console.WriteLine();
         }
 
-        public void AddMeat()
+        public override void AddMeat()
         {
+            Console.Write($"Adding the meat:");
 
+            foreach(string meat in meatUsed)
+            {
+                Console.Write($" {meat}");
+            }
+
+            Console.WriteLine();
         }
 
-        public void AddCheese()
+        public override void AddVegetables()
         {
+            Console.Write($"Adding the vegetables:");
+
+            foreach (string veg in veggiesUsed)
+            {
+                Console.Write($" {veg}");
+            }
+
+            Console.WriteLine();
 
         }
-
-        public void AddVegetables()
-        {
-
-
-        }
-
-        public void AddCondiments()
-        {
-
-        }
-
-        public void WrapTheHoagie()
-        {
-
-        }
-
     }
 }
