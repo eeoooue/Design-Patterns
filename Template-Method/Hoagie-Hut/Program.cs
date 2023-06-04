4d9f1e6d65e777dba941b6790a5f38f16666b601
eeoooue <pdmoorhouse@gmail.com>
2023-06-04 05:45:09 +0100

    created new Hoagie-Hut project following Derek Banas' video
    video: https://www.youtube.com/watch?v=aR1B8MlwbRI

---
+++ Template-Method/Hoagie-Hut/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hoagie_Hut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
