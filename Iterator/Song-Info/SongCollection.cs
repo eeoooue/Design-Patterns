28b937dbede4b6cbaceff364ddb4c0ac0af08b04
eeoooue <pdmoorhouse@gmail.com>
2023-06-05 05:24:36 +0100

    renamed SongIterator to SongCollection as classes that implement this interface have a SongIterator

---
+++ Iterator/Song-Info/SongCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Song_Info
+{
+    internal interface SongCollection
+    {
+        public Iterator CreateIterator();
+    }
+}
