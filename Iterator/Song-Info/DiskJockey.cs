8042831edd321b18c698f9b46c94bafab5a3cb08
eeoooue <pdmoorhouse@gmail.com>
2023-06-05 05:09:48 +0100

    created a DiskJockey class which displays songs from the song collections

---
+++ Iterator/Song-Info/DiskJockey.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Song_Info
+{
+    internal class DiskJockey
+    {
+        SongsOfThe70s _songs70s;
+        SongsOfThe80s _songs80s;
+        SongsOfThe90s _songs90s;
+
+        public DiskJockey(SongsOfThe70s songs70s, SongsOfThe80s songs80s, SongsOfThe90s songs90s)
+        {
+            _songs70s = songs70s;
+            _songs80s = songs80s;
+            _songs90s = songs90s;
+        }
+
+        public void ShowTheSongs()
+        {
+            ShowBest70s();
+            ShowBest80s();
+            ShowBest90s();
+        }
+
+        private void ShowBest70s()
+        {
+            List<SongInfo> songs = _songs70s.getBestSongs();
+
+            foreach (SongInfo song in songs)
+            {
+                Console.WriteLine(song);
+            }
+        }
+
+        private void ShowBest80s()
+        {
+            SongInfo[] songs = _songs80s.getBestSongs();
+
+            foreach (SongInfo song in songs)
+            {
+                Console.WriteLine(song);
+            }
+        }
+
+        private void ShowBest90s()
+        {
+            Dictionary<int, SongInfo> songs = _songs90s.getBestSongs();
+
+            foreach (SongInfo song in songs.Values)
+            {
+                Console.WriteLine(song);
+            }
+        }
+    }
+}
--- Iterator/Song-Info/Program.cs
+++ Iterator/Song-Info/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            SongsOfThe70s songs70s = new SongsOfThe70s();
+            SongsOfThe80s songs80s = new SongsOfThe80s();
+            SongsOfThe90s songs90s = new SongsOfThe90s();
+
+            DiskJockey dj = new DiskJockey(songs70s, songs80s, songs90s);
+
+            dj.ShowTheSongs();
         }
     }
 }
