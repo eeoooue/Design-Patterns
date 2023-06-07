761f9fb1f0a41810a5fcc6342a6c290300476d96
eeoooue <pdmoorhouse@gmail.com>
2023-06-07 05:09:08 +0100

    created new Logistics-App project to follow Refactoring Guru example
    trying this example: https://refactoring.guru/design-patterns/factory-method

---
+++ Factory-Method/Logistics-App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logistics_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
