1034baec8e17af11bd30f0a93cbd7b8c56e6f963
eeoooue <pdmoorhouse@gmail.com>
2023-06-07 05:33:09 +0100

    created Ship and Truck classes, our ConcreteProducts

---
+++ Factory-Method/Logistics-App/Transports/Truck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistics_App.Transports
+{
+    internal class Truck : Transport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine($"The truck takes the delivery along roads to its destination.");
+        }
+    }
+}
