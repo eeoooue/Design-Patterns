3fb50c626044108ea768186ca9aa0d81a1095a74
eeoooue <pdmoorhouse@gmail.com>
2023-06-07 05:33:55 +0100

    created RoadLogistics and SeaLogistics, our ConcreteCreators

---
+++ Factory-Method/Logistics-App/LogisticsTypes/SeaLogistics.cs
@@ -0,0 +1,24 @@
+﻿using Logistics_App.Transports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistics_App.LogisticsTypes
+{
+    internal class SeaLogistics : Logistics
+    {
+        public Transport CreateTransport()
+        {
+            return new Ship();
+        }
+
+        public void PlanDelivery()
+        {
+            Console.WriteLine($"The delivery will be made by ship.");
+            Transport ship = CreateTransport();
+            ship.Deliver();
+        }
+    }
+}
