24c012c858686cae4838b4f5d563fbca25394636
eeoooue <pdmoorhouse@gmail.com>
2023-06-07 05:33:29 +0100

    created Logistics interface, our Creator

---
+++ Factory-Method/Logistics-App/Logistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistics_App
+{
+    interface Logistics
+    {
+        public abstract void PlanDelivery();
+
+        public abstract Transport CreateTransport();
+    }
+}
