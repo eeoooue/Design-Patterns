d22b1a9d2d2808308190349a678caa56db0fcef9
eeoooue <pdmoorhouse@gmail.com>
2023-06-08 05:37:33 +0100

    ConcreteDecorators call the wrappee Send() behaviour and include their own behaviours

---
+++ Decorator/Notification-Library/Decorators/SMSDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification_Library.Decorators
+{
+    internal class SMSDecorator : Decorator
+    {
+        public SMSDecorator(Notifier wrappee) : base(wrappee) { }
+
+        public override void Send()
+        {
+            Console.WriteLine($"SMS: {Wrappee.Message}");
+            Wrappee.Send();
+        }
+    }
+}
---
+++ Decorator/Notification-Library/Decorators/SlackDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification_Library.Decorators
+{
+    internal class SlackDecorator : Decorator
+    {
+        public SlackDecorator(Notifier wrappee): base(wrappee) { }
+
+        public override void Send()
+        {
+            Console.WriteLine($"Slack: {Wrappee.Message}");
+            Wrappee.Send();
+        }
+    }
+}
