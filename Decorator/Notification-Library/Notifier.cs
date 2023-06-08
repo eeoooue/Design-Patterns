8b440aa0f69837b7c6bb7e12714a1bf39d97ca17
eeoooue <pdmoorhouse@gmail.com>
2023-06-08 05:21:20 +0100

    created Notifier, our Component

---
+++ Decorator/Notification-Library/Notifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification_Library
+{
+    internal class Notifier
+    {
+        public string Message { get; private set; }
+
+        public Notifier(string message)
+        {
+            Message = message;
+        }
+    }
+}
