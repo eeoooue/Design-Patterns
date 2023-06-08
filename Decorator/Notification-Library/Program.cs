ddac8617084791d0a4464b48c5337372c3fe7844
eeoooue <pdmoorhouse@gmail.com>
2023-06-08 05:37:46 +0100

    demonstration of the Notifier system

--- Decorator/Notification-Library/Program.cs
+++ Decorator/Notification-Library/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Notification_Library
+﻿using Notification_Library.Decorators;
+
+namespace Notification_Library
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Notifier notif = new Notifier("You have a message!");
+            notif = new FacebookDecorator(notif);
+            notif = new SlackDecorator(notif);
+            notif = new SMSDecorator(notif);
+
+            notif.Send();
         }
     }
 }
