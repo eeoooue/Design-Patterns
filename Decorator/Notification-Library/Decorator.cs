ad0cdfa05418b44aa7b2db51c35e37896229c7c7
eeoooue <pdmoorhouse@gmail.com>
2023-06-08 05:26:33 +0100

    created abstract base Decorator

---
+++ Decorator/Notification-Library/Decorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification_Library
+{
+    abstract class Decorator
+    {
+        private Notifier Wrappee { get; set; }
+
+        public Decorator(Notifier wrappee)
+        {
+            Wrappee = wrappee;
+        }
+        public abstract void Send();
+    }
+}
