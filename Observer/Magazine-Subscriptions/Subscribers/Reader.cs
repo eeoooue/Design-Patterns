88409b74f4e39a40ec55ff02729969a891d79aed
eeoooue <pdmoorhouse@gmail.com>
2023-06-09 14:31:22 +0100

    Reader is our ConcreteObserver / ConcreteSubscriber

---
+++ Observer/Magazine-Subscriptions/Subscribers/Reader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine_Subscriptions.Subscribers
+{
+    internal class Reader : Subscriber
+    {
+        public string Name { get; private set; }
+
+        public Reader(string name)
+        {
+            Name = name;
+        }
+
+        public void Update(Publisher publisher)
+        {
+            Console.WriteLine($"{Name} was notified by the publisher");
+        }
+    }
+}
