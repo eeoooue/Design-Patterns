ae9b78b58aa0d47cda93d20a151977b85799df8f
eeoooue <pdmoorhouse@gmail.com>
2023-06-09 14:31:45 +0100

    wrote some demo code to show how Subscribers can subscribe/unsubscribe to the notification system

--- Observer/Magazine-Subscriptions/Program.cs
+++ Observer/Magazine-Subscriptions/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Magazine_Subscriptions
+﻿using Magazine_Subscriptions.Subscribers;
+
+namespace Magazine_Subscriptions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Publisher publisher = new Publisher();
+            Reader henry = new Reader("Henry");
+            Reader sophie = new Reader("Sophie");
+            Reader james = new Reader("James");
+            Reader alice = new Reader("Alice");
+
+            Console.WriteLine($"At first, Henry, Sophie & Alice subscribe to the publisher.");
+            publisher.Subscribe(henry);
+            publisher.Subscribe(sophie);
+            publisher.Subscribe(james);
+            Console.WriteLine();
+
+            Console.WriteLine($"When a magazine is published:");
+            publisher.NotifySubscribers();
+            Console.WriteLine();
+
+            Console.WriteLine($"Alice subscribes.");
+            publisher.Subscribe(alice);
+            Console.WriteLine($"James unsubscribes.");
+            publisher.Unsubscribe(james);
+            Console.WriteLine();
+
+            Console.WriteLine($"When a magazine is published:");
+            publisher.NotifySubscribers();
+            Console.WriteLine();
         }
     }
 }
