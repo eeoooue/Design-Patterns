2bea8e86b32d268d0b97d3348ca797adea51a315
eeoooue <pdmoorhouse@gmail.com>
2023-06-11 10:31:02 +0100

    wrote up a Route-Planning strategy pattern based on Dive into Design Patterns example

---
+++ Strategy/Route-Planning/RouteStrategies/RoadStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route_Planning.RouteStrategies
+{
+    internal class RoadStrategy : RouteStrategy
+    {
+        public void BuildRoute(string start, string destination)
+        {
+            Console.WriteLine($"Built a route for driving from {start} to {destination}");
+        }
+    }
+}
