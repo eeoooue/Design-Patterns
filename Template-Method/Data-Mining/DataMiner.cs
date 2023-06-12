d492518a286bf8d4f1e52a076abc8784a4d67749
eeoooue <pdmoorhouse@gmail.com>
2023-06-12 10:52:06 +0100

    wrote-up a "Data-Mining" example from Dive into Design Patterns

---
+++ Template-Method/Data-Mining/DataMiner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Mining
+{
+    public abstract class DataMiner
+    {
+        public void Mine(string path)
+        {
+            OpenFile();
+            ExtractData();
+            ParseData();
+            AnalyzeData();
+            SendReport();
+            CloseFile();
+        }
+
+        protected abstract void OpenFile();
+
+        protected abstract void ExtractData();
+
+        protected abstract void ParseData();
+
+        protected void AnalyzeData()
+        {
+            Console.WriteLine($"Analysing the parsed data.");
+        }
+
+        protected void SendReport()
+        {
+            Console.WriteLine($"Sending the analysis report.");
+        }
+
+        protected abstract void CloseFile();
+    }
+}
---
+++ Template-Method/Data-Mining/Program.cs
@@ -0,0 +1,13 @@
+﻿using Data_Mining.DataMiners;
+
+namespace Data_Mining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMiner miner = new PDFDataMiner();
+            miner.Mine("example.pdf");
+        }
+    }
+}
