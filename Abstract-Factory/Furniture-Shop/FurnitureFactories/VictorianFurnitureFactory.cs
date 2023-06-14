c038e95d6d9b312dbe51accb9834d09d495aa475
eeoooue <pdmoorhouse@gmail.com>
2023-06-14 19:57:45 +0100

    wrote up a Furniture Shop example as outlined in Dive into Design Patterns

---
+++ Abstract-Factory/Furniture-Shop/FurnitureFactories/VictorianFurnitureFactory.cs
@@ -0,0 +1,29 @@
+﻿using Furniture_Shop.Chairs;
+using Furniture_Shop.CoffeeTables;
+using Furniture_Shop.Sofas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furniture_Shop.FurnitureFactories
+{
+    internal class VictorianFurnitureFactory : FurnitureFactory
+    {
+        public Chair CreateChair()
+        {
+            return new VictorianChair();
+        }
+
+        public CoffeeTable CreateCoffeeTable()
+        {
+            return new VictorianCoffeeTable();
+        }
+
+        public Sofa CreateSofa()
+        {
+            return new VictorianSofa();
+        }
+    }
+}
---
+++ Abstract-Factory/Furniture-Shop/Program.cs
@@ -0,0 +1,15 @@
+﻿using Furniture_Shop.FurnitureFactories;
+
+namespace Furniture_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FurnitureFactory factory = new VictorianFurnitureFactory();
+
+            Chair chair = factory.CreateChair();
+            Console.WriteLine(chair);
+        }
+    }
+}
