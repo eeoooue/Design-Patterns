909f95f283e2033354d5e7d3403af2347872da01
eeoooue <pdmoorhouse@gmail.com>
2023-06-15 18:23:36 +0100

    started new Tree Traversal example from Dive into Design Patterns

---
+++ Iterator/Tree-Traversal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tree_Traversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
