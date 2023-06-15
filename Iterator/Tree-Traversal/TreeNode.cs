42d349314f1487f11491ab6d47f1732cba699791
eeoooue <pdmoorhouse@gmail.com>
2023-06-15 18:25:35 +0100

    created a TreeNode class

---
+++ Iterator/Tree-Traversal/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_Traversal
+{
+    internal class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode? Left { get; set; }
+        public TreeNode? Right { get; set; }
+
+        public TreeNode(int val=0)
+        {
+            Val = val;
+            Left = null;
+            Right = null;
+        }
+    }
+}
