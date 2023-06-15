472514e2407e50fb494864f575517bb088642058
eeoooue <pdmoorhouse@gmail.com>
2023-06-15 18:27:01 +0100

    outlined a TreeCollection class

---
+++ Iterator/Tree-Traversal/TreeCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_Traversal
+{
+    internal class TreeCollection
+    {
+        private TreeNode _root;
+
+        public TreeCollection(TreeNode root)
+        {
+            _root = root;
+        }
+
+        public Iterator GetDepthIterator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Iterator GetBreadthIterator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
